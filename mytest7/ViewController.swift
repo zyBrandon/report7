443e5913c4f9038c3ffab2362f64b1ddfb041ab2
apple <apple@jeromedeimac.local>
2018-12-16 20:09:43 +0800

    Initial Commit

---
+++ mytest7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mytest7
+//
+//  Created by apple on 2018/12/16.
+//  Copyright © 2018年 李志明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
